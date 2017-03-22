c7a84da22468cbdf9dad67542e00d2c894902f05
Slynchy <sam.lynch2@hotmail.co.uk>
2017-03-22 13:48:20 +0000

    Added background loading/drawing

---
+++ Assets/Scripts/BackgroundMaster.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace M22
+{
+    class BackgroundMaster : MonoBehaviour
+    {
+        static Dictionary<string, Sprite> loadedBackgrounds;
+
+        void Awake()
+        {
+            loadedBackgrounds = new Dictionary<string, Sprite>();
+        }
+
+        public static bool LoadBackground(string name)
+        {
+            string filename = "./Backgrounds/" + name;
+            Texture2D temp = Resources.Load(filename) as Texture2D;
+            if (temp)
+            {
+                Sprite tempSpr = Sprite.Create(temp, new Rect(0, 0, temp.width, temp.height), new Vector2(0, 0));
+                loadedBackgrounds.Add(name, tempSpr);
+                return true;
+            }
+            else return false;
+        }
+
+        static public Sprite GetBackground(string file)
+        {
+            if (loadedBackgrounds.ContainsKey(file))
+            {
+                Sprite returnVal;
+                loadedBackgrounds.TryGetValue(file, out returnVal);
+                return returnVal;
+            }
+            else return new Sprite();
+        }
+
+        void UnloadBackgrounds()
+        {
+            loadedBackgrounds.Clear();
+        }
+    }
+}
