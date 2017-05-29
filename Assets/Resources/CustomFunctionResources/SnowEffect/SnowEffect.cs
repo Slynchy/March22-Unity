87f613da163c48eeba7fe0573fd6a6adddbf9cb5
Sam Lynch <sam.lynch2@hotmail.co.uk>
2017-05-29 19:38:06 +0100

    Added SnowEffect custom function

---
+++ Assets/Resources/CustomFunctionResources/SnowEffect/SnowEffect.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CustomFunctions
+{
+    public class SnowEffect : CustomFunction
+    {
+        GameObject SnowEffectPrefab;
+        GameObject SnowEffectInstance;
+        SnowEffectObjectScript SnowEffectScript;
+
+        public override void Awake()
+        {
+            SnowEffectPrefab = Resources.Load<GameObject>("CustomFunctionResources/SnowEffect/SnowEffectPrefab") as GameObject;
+        }
+
+        public override void Func(string param1, string param2, string param3)
+        {
+            bool stopOrStart = false; // false == stop, true == start
+            if (param1.Equals("start"))
+                stopOrStart = true;
+            else if (param1.Equals("stop"))
+                stopOrStart = false;
+            else
+            {
+                Debug.LogError("Invalid parameter on SnowEffect function!");
+                Camera.main.GetComponent<M22.ScriptMaster>().NextLine();
+                return;
+            }
+
+            // starting
+            if(stopOrStart == true)
+            {
+                SnowEffectInstance = GameObject.Instantiate<GameObject>(SnowEffectPrefab, Camera.main.GetComponentInChildren<Canvas>().transform);
+                SnowEffectScript = SnowEffectInstance.GetComponent<SnowEffectObjectScript>();
+            }
+            else // stopping
+            {
+                if (SnowEffectInstance == null || SnowEffectScript == null)
+                {
+                    Debug.LogError("Trying to stop SnowEffect when there isn't one!");
+                    Camera.main.GetComponent<M22.ScriptMaster>().NextLine();
+                    return;
+                }
+                SnowEffectScript.Stop();
+            }
+
+            Camera.main.GetComponent<M22.ScriptMaster>().NextLine();
+
+        }
+    }
+}
---
+++ Assets/Resources/CustomFunctionResources/SnowEffect/SnowEffectObjectScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SnowEffectObjectScript : MonoBehaviour
+{
+    bool stopping = false;
+
+    Material particleMat;
+
+    const float stopSpeed = 0.5f;
+
+    void Start ()
+    {
+        particleMat = /*GetComponent<ParticleSystem>().*/GetComponent<Renderer>().material;
+	}
+	
+	void Update ()
+    {
+        if(stopping == true)
+        {
+            if(particleMat.GetColor("_TintColor").a > 0)//particleMat.color.a > 0)
+            {
+                particleMat.SetColor(
+                    "_TintColor", 
+                    new Color(1, 1, 1,
+                        particleMat.GetColor("_TintColor").a - (stopSpeed * Time.deltaTime)
+                    )
+                );
+            }
+            else
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+        }
+	}
+
+    public void Stop()
+    {
+        stopping = true;
+    }
+}
