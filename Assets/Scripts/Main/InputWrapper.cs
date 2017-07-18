bd7c7e1ad186220ece0582eeaf836a0ba4862e5a
Sam Lynch <sam.lynch2@hotmail.co.uk>
2017-07-18 02:54:48 +0100

    Optimized mobile compilation settings

--- Assets/Scripts/Main/InputWrapper.cs
+++ Assets/Scripts/Main/InputWrapper.cs
@@ -7,6 +7,7 @@ namespace M22
 
     public class InputWrapper
     {
+        static bool stillActiveFromPreviousFrame = false;
 
         static public bool NextLineButton()
         {
@@ -17,7 +18,45 @@ namespace M22
                 case RuntimePlatform.WebGLPlayer:
                 case RuntimePlatform.LinuxEditor:
                 case RuntimePlatform.LinuxPlayer:
+                case RuntimePlatform.OSXDashboardPlayer:
+                case RuntimePlatform.OSXEditor:
+                case RuntimePlatform.OSXPlayer:
                     return Input.GetKeyDown(KeyCode.Return);
+                case RuntimePlatform.Android:
+                case RuntimePlatform.IPhonePlayer:
+                case RuntimePlatform.Switch:
+                case RuntimePlatform.PSP2:
+                    if (Input.touchCount > 0)
+                    {
+                        var temp = Input.GetTouch(0).position;
+                        if(temp.x > Screen.width * 0.3f && temp.x < (Screen.width - (Screen.width * 0.3f)))
+                        {
+                            if (temp.y > Screen.height * 0.3f && temp.y < (Screen.height - (Screen.height * 0.3f)))
+                            {
+                                if(stillActiveFromPreviousFrame == false)
+                                {
+                                    stillActiveFromPreviousFrame = true;
+                                    return true;
+                                }
+                                    return false;
+                            }
+                            else
+                            {
+                                stillActiveFromPreviousFrame = false;
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            stillActiveFromPreviousFrame = false;
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        stillActiveFromPreviousFrame = false;
+                        return false;
+                    }
                 default:
                     return false;
             }
@@ -32,7 +71,20 @@ namespace M22
                 case RuntimePlatform.WebGLPlayer:
                 case RuntimePlatform.LinuxEditor:
                 case RuntimePlatform.LinuxPlayer:
+                case RuntimePlatform.OSXDashboardPlayer:
+                case RuntimePlatform.OSXEditor:
+                case RuntimePlatform.OSXPlayer:
                     return (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift));
+                case RuntimePlatform.Android:
+                case RuntimePlatform.IPhonePlayer:
+                case RuntimePlatform.Switch:
+                case RuntimePlatform.PSP2:
+                    if (Input.touchCount == 2)
+                    {
+                        return true;
+                    }
+                    else
+                        return false;
                 default:
                     return false;
             }
