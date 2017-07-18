93123e53bc4794b525fd94bff9eb684dbf369ce7
Sam Lynch <sam.lynch2@hotmail.co.uk>
2017-07-18 01:49:23 +0100

    Added an input wrapper for multi-platform

---
+++ Assets/Scripts/Main/InputWrapper.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace M22
+{
+
+    public class InputWrapper
+    {
+
+        static public bool NextLineButton()
+        {
+            switch(Application.platform)
+            {
+                case RuntimePlatform.WindowsPlayer:
+                case RuntimePlatform.WindowsEditor:
+                case RuntimePlatform.WebGLPlayer:
+                case RuntimePlatform.LinuxEditor:
+                case RuntimePlatform.LinuxPlayer:
+                    return Input.GetKeyDown(KeyCode.Return);
+                default:
+                    return false;
+            }
+        }
+
+        static public bool SkipTextButton()
+        {
+            switch (Application.platform)
+            {
+                case RuntimePlatform.WindowsPlayer:
+                case RuntimePlatform.WindowsEditor:
+                case RuntimePlatform.WebGLPlayer:
+                case RuntimePlatform.LinuxEditor:
+                case RuntimePlatform.LinuxPlayer:
+                    return (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift));
+                default:
+                    return false;
+            }
+        }
+
+    }
+
+}
