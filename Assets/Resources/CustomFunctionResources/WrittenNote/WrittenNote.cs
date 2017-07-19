c5f9efec45e8e1db25129bddf52c1b58b2900992
Sam Lynch <sam.lynch2@hotmail.co.uk>
2017-07-19 17:07:24 +0100

    Finished adding WrittenNote functionality

--- Assets/Resources/CustomFunctionResources/WrittenNote/WrittenNote.cs
+++ Assets/Resources/CustomFunctionResources/WrittenNote/WrittenNote.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace M22
 {
@@ -9,15 +10,13 @@ namespace M22
     {
         public class WrittenNote : CustomFunction
         {
-            Sprite NoteSprite;
             GameObject WrittenNoteInstance;
             GameObject WrittenNotePrefab;
             ScriptMaster ScriptMaster;
 
             public override void Awake()
             {
-                WrittenNotePrefab = Resources.Load<GameObject>("CustomFunctionResources/WrittenNote/WrittenNotePrefab") as GameObject;
-                NoteSprite = Resources.Load<Sprite>("CustomFunctionResources/WrittenNote/WrittenNotePrefab");
+                WrittenNotePrefab = Resources.Load<GameObject>("CustomFunctionResources/WrittenNote/NotePrefab") as GameObject;
             }
 
             public override void Start()
@@ -25,12 +24,29 @@ namespace M22
                 ScriptMaster = Camera.main.GetComponent<M22.ScriptMaster>();
             }
 
+            // param1 = in or out
+            // param2 = text, delimited by underscores
             public override void Func(string param1, string param2, string param3)
             {
-                string param_fixed = param1.Replace('_', ' ');
-                WrittenNoteInstance = GameObject.Instantiate<GameObject>(WrittenNotePrefab, ScriptMaster.GetCanvas(M22.ScriptMaster.CANVAS_TYPES.POSTCHARACTER).transform);
-
-                ScriptMaster.NextLine();
+                if(param1.Equals("out"))
+                {
+                    if (WrittenNoteInstance == null)
+                    {
+                        UnityWrapper.LogWarningFormat("WrittenNoteInstance is null; trying to fade out when there isn't a note?");
+                    }
+                    else
+                    {
+                        WrittenNoteInstance.GetComponent<WrittenNoteObjectScript>().HideNote();
+                        WrittenNoteInstance = null;
+                    }
+                }
+                else
+                {
+                    string param2_fixed = param2.Replace('_', ' ');
+                    param2_fixed = param2_fixed.Replace("\\n", "\n");
+                    WrittenNoteInstance = GameObject.Instantiate<GameObject>(WrittenNotePrefab, ScriptMaster.GetCanvas(M22.ScriptMaster.CANVAS_TYPES.POSTCHARACTER).transform);
+                    WrittenNoteInstance.GetComponentInChildren<Text>().text = param2_fixed;
+                }
             }
         }
     }
